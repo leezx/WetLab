ef8b7ab54f5d3fd28616a5089c716cfbdd2bff5c
Zhixin Eason Li <zxlee@connect.hku.hk>
2025-10-06 11:48:07 -0400

    update

---
+++ 2.Cloning/Plasmids Maps/GeneCopoeia SOX9 order_files/popup.css.php
@@ -0,0 +1,63 @@
+
+
+.modal-header {
+	background: #204d74;
+}
+.close {
+	font-weight: normal;
+	color: #fff;
+	filter: alpha(opacity=100);
+	opacity: 1;
+}
+.modal-title{
+	font-size: 18px;
+	color:#fff;
+}
+
+.popup_info{
+	width: auto;
+	margin: 20px auto;
+	font-size: 16px;
+	border-radius: 6px;
+	padding: 20px 40px;
+	max-width: 600px;
+}
+.popup_info h1{
+	font-size: 36px;
+	font-weight: 500;
+	line-height: 1.1;
+	text-align: center;
+}
+
+.popup_info .form-control{
+	height: 20px;
+}
+.popup_info .line1{
+	margin: 30px 0 10px 0;
+}
+.popup_info .line2{
+	margin: 10px 0 30px 16px;
+}
+.popup_info ul.line2 li{
+	line-height: 26px;
+}
+.popup_info .line3{
+}
+
+.popup_info .line3 input[name=log_email]{
+	width: 360px;
+}
+.popup_info .line4{
+	margin: 30px 0;
+}
+.popup_info .line4 a{
+	color: #31708f;
+	font-weight: bold;
+	text-decoration: underline;
+}
+.popup_info .line5{
+	font-size: 12px;
+}
+.modal-footer {
+	background: #204d74;
+}
